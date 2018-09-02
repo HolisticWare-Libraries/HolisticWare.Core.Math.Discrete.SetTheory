fa0800f8bfbb7a8b5829eb056aa8a7f91c397b7c
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-09-02 23:31:24 +0200

    initial commit

---
+++ source/HolisticWare.Core.Math.SetTheory/Set.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Core.Math.SetTheory
+{
+    public partial class Set<T>
+        :
+        System.Collections.Generic.IEnumerable<T>
+    {
+        // ctor = constructor
+        public Set()
+            : base()
+        {
+            return;
+        }
+
+        public Set(IEnumerable<T> ie)
+            : base(ie)
+        {
+            return;
+        }
+
+        // intersection         - operator *   (logical AND )
+        // union                - operator + 
+        // Cartersian product   - operator % 
+        // negation ??
+
+        public static Set<T> operator +(Set<T> lhs, Set<T> rhs)
+        {
+            Set<T> result = new Set<T>();
+            result.AddRange(lhs);
+            result.AddRange(rhs);
+
+            result = new Set<T>(result.Distinct());
+
+            return result;
+        }
+
+    }
+
+
+
+
+
+}
